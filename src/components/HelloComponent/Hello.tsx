91b2bbb4005f4773aec0bc60e5684ed837dc43b9
sharthak123 <sayanmallik1995@gmail.com>
2021-07-28 22:23:58 +0530

    add lint-staged and prettier to the project

--- src/components/HelloComponent/Hello.tsx
+++ src/components/HelloComponent/Hello.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Hello = () => {
-    return (
-        <div>
-            Hello World !
-        </div>
-    )
-}
+  return <div>Hello World !</div>;
+};
 
 export default Hello;
